8393b8dcfa0cc4ffef6d658f22e7535c90dd58da
Amelia <amelia.holcomb@gmail.com>
2023-09-28 21:56:05 +0000

    [fix]: get env vars or fail

--- gedidb/environment.py
+++ gedidb/environment.py
@@ -20,17 +20,17 @@ LOG_PATH = PROJECT_PATH / "logs"
 LOG_PATH.mkdir(parents=True, exist_ok=True)
 
 #  Data related paths
-DATA_PATH = Path(os.getenv("DATA_PATH"))
-USER_PATH = Path(os.getenv("USER_PATH"))
+DATA_PATH = Path(os.environ["DATA_PATH"])
+USER_PATH = Path(os.environ["USER_PATH"])
 EARTHDATA_USER = os.getenv("EARTHDATA_USER")
 EARTHDATA_PASSWORD = os.getenv("EARTHDATA_PASSWORD")
-EARTH_DATA_COOKIE_FILE = Path(os.getenv("EARTH_DATA_COOKIE_FILE"))
+EARTH_DATA_COOKIE_FILE = Path(os.environ["EARTH_DATA_COOKIE_FILE"])
 
 GEDI_PATH = DATA_PATH / "GEDI"
 
 
-def gedi_product_path(product):
-	return GEDI_PATH / product.value
+def gedi_product_path(product: GediProduct) -> Path:
+    return GEDI_PATH / product.value
 
 
 GEDI_L1B_PATH = gedi_product_path(GediProduct.L1B)
@@ -44,10 +44,10 @@ ENV_VARS_NAMES = ["defMean", "SCCsoil", "fpar", "lightning", "srtm"]
 
 # ---------------- LOGGING CONSTANTS ----------------
 DEFAULT_FORMATTER = logging.Formatter(
-	(
-		"%(asctime)s %(levelname)s: %(message)s "
-		"[in %(funcName)s at %(pathname)s:%(lineno)d]"
-	)
+    (
+        "%(asctime)s %(levelname)s: %(message)s "
+        "[in %(funcName)s at %(pathname)s:%(lineno)d]"
+    )
 )
 DEFAULT_LOG_FILE = LOG_PATH / "default_log.log"
 DEFAULT_LOG_LEVEL = logging.INFO  # verbose logging per default
@@ -60,6 +60,4 @@ DB_NAME = os.getenv("DB_NAME")  # Database for GEDI shots
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_POSTGRES = "postgresql"
-DB_CONFIG = (
-	f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-)
+DB_CONFIG = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
