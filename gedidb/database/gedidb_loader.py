9e4b4c8aed5955c264d7ff5eb2322eb3e08031de
Amelia <amelia.holcomb@gmail.com>
2023-09-28 22:06:33 +0000

    [feat] parse granule files for db insertion

---
+++ gedidb/database/gedidb_loader.py
@@ -0,0 +1,79 @@
+from typing import Optional
+from tqdm.auto import tqdm
+import geopandas as gpd
+import pandas as pd
+from pathlib import Path
+import warnings
+
+from gedidb.granule.gedi_granule import GediGranule  # for typing only
+from gedidb.granule.gedi_l4a import L4AGranule
+from gedidb.granule.gedi_l2b import L2BGranule
+from gedidb.granule.gedi_l2a import L2AGranule
+from gedidb.constants import WGS84, GediProduct
+
+from typing import Union
+
+
+def parse_file(
+    product: Union[str, GediProduct], file: Path, quality_filter=True
+) -> gpd.GeoDataFrame:
+    product = GediProduct(product)
+    if product == GediProduct.L4A:
+        return parse_file_l4a(file, quality_filter)
+    elif product == GediProduct.L2B:
+        return parse_file_l2b(file, quality_filter)
+    elif product == GediProduct.L2A:
+        return parse_file_l2a(file, quality_filter)
+    else:
+        raise ValueError(f"Product {product} not supported")
+
+
+def parse_file_l4a(file: Path, quality_filter=True) -> gpd.GeoDataFrame:
+    granule = L4AGranule(file)
+    return _parse(granule, quality_filter)
+
+
+def parse_file_l2b(file: Path, quality_filter=True) -> gpd.GeoDataFrame:
+    granule = L2BGranule(file)
+    return _parse(granule, quality_filter)
+
+
+def parse_file_l2a(file: Path, quality_filter=True) -> gpd.GeoDataFrame:
+    granule = L2AGranule(file)
+    return _parse(granule, quality_filter)
+
+
+def _parse(granule: GediGranule, quality_filter=True) -> gpd.GeoDataFrame:
+    granule_data = []
+    for beam in tqdm(granule.iter_beams(), total=granule.n_beams):
+        print(beam)
+        print(len(beam.main_data))
+        try:
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")
+                if quality_filter:
+                    beam.quality_filter()
+                    print(len(beam.main_data))
+                beam.sql_format_arrays()
+                print(len(beam.main_data))
+                granule_data.append(beam.main_data)
+        except KeyError as e:
+            print(e)
+            continue
+    df = pd.concat(granule_data, ignore_index=True)
+    print(len(df))
+    gdf = gpd.GeoDataFrame(df, crs=WGS84)
+    granule.close()
+    return gdf
+
+
+def spatial_filter_granules(
+    gdf: gpd.GeoDataFrame, roi: Optional[gpd.GeoDataFrame]
+) -> gpd.GeoDataFrame:
+    if roi is None:
+        return gdf
+
+    # Filter for shots that fall within the ROI
+    gdf_filtered = gpd.sjoin(gdf, roi, predicate="within", how="inner")
+    gdf_filtered = gdf_filtered.drop("index_right", axis=1)
+    return gdf_filtered
